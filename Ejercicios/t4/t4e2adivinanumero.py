6cd57d8447e99dc0b4fc6e70ae45786c83118072
Grimanesa <grimanesa.lozano83@gmail.com>
2026-04-24 10:08:49 +0100

    ejercicio t4e2

---
+++ Ejercicios/t4/t4e2adivinanumero.py
@@ -0,0 +1,11 @@
+
+def eligenumero(numero):
+    if (numero == 4):
+        mensaje = "Victoria, has acertado"
+    if (numero != 4):
+        mensaje = "Derrota, has fallado"
+    return mensaje
+
+numero = int(input("Elige un número entre 1 y 10:" ))
+mensaje = eligenumero(numero)
+print(mensaje)
